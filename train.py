a1851f872c4bd168b95108d5e976e6431c647b8e
ChanYoung <young221718@gmail.com>
2022-09-28 08:34:40 +0900

    update

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def train(model, train_loader, val_loader, optimizer, criterion, scheduler, devi
     for epoch in range(1, max_epoch):
         # training
         model.train()
-        train_loss = []
+        train_loss_list = []
         for lr_img, hr_img in tqdm(iter(train_loader)):
             lr_img, hr_img = lr_img.float().to(device), hr_img.float().to(device)
 
@@ -30,34 +30,38 @@ def train(model, train_loader, val_loader, optimizer, criterion, scheduler, devi
             loss.backward()
             optimizer.step()
 
-            train_loss.append(loss.item())
+            train_loss_list.append(loss.item())
 
         if scheduler is not None:
             scheduler.step()
 
-        _train_loss = np.mean(train_loss)
+        _train_loss = np.mean(train_loss_list)
 
         # validation
+        val_loss_list = []
         if val_loader:
             with torch.no_grad():
                 model.eval()
-                val_loss = []
+                
                 for lr_img, hr_img in tqdm(iter(val_loader)):
                     lr_img, hr_img = lr_img.float().to(device), hr_img.float().to(device)
 
                     pred_hr_img = model(lr_img)
                     loss = criterion(pred_hr_img, hr_img)
 
-                    val_loss.append(loss.item())
+                    val_loss_list.append(loss.item())
 
-                _val_loss = np.mean(val_loss)
+                _val_loss = np.mean(val_loss_list)
 
                 if best_loss > _val_loss:
                     best_loss = _val_loss
                     best_model = model
-                    torch.save(model.state_dict(), save_path)
+                    torch.save({"state_dict": model.module.state_dict(),
+                                'loss': _val_loss,
+                                'epoch': epoch},
+                               save_path)
                     check_early_stop = 0
-                    print("Minist Val Loss")
+                    # print("Minist Val Loss")
                 else:
                     # implement early stop
                     check_early_stop += 1
@@ -67,7 +71,10 @@ def train(model, train_loader, val_loader, optimizer, criterion, scheduler, devi
             if best_loss > _train_loss:
                 best_loss = _train_loss
                 best_model = model
-                torch.save(model.state_dict(), save_path)
+                torch.save({"state_dict": model.module.state_dict(),
+                            'loss': _val_loss,
+                            'epoch': epoch},
+                           save_path)
                 check_early_stop = 0
                 print("Minist Trian Loss")
             else:
@@ -80,5 +87,3 @@ def train(model, train_loader, val_loader, optimizer, criterion, scheduler, devi
             f'Epoch:{epoch}, Train Loss:{_train_loss:.5f}, Val Loss:{_val_loss:.5f}, es:{check_early_stop}')
 
     return best_model
-
-
