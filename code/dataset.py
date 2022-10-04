e662097ceb6636da2b740efbc7f6ee8430081cf5
ChanYoung <young221718@gmail.com>
2022-10-04 18:57:33 +0900

    code modify and training for submit

--- code/dataset.py
+++ code/dataset.py
@@ -3,17 +3,21 @@ import os
 from torch.utils.data import Dataset, DataLoader
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
-
+import numpy as np
 from PIL import Image
 
 class CustomDataset(Dataset):
-    def __init__(self, df, transforms, mode, root_path, hr_img_size = 512, lr_img_size = 128):
+    def __init__(self, df, mode, root_path, hr_size = 512, lr_size = 128):
         self.df = df
-        self.transforms = transforms
         self.mode = mode
         self.root_path = root_path
-        self.lr_img_size = lr_img_size
-        self.hr_img_size = hr_img_size
+        self.lr_size = lr_size
+        self.hr_size = hr_size
+
+        if mode == "train":
+            self.transforms = get_train_transform()
+        else:
+            self.transforms = get_test_transform()
 
     def __getitem__(self, index):
         
@@ -21,50 +25,64 @@ class CustomDataset(Dataset):
             # train or val mode learning with hr == 512*512, lr == 128*128
             hr_path = os.path.join(self.root_path, self.df['HR'].iloc[index])
             hr_img = cv2.imread(hr_path)
-            hr_img = cv2.resize(hr_img, (self.hr_img_size, self.hr_img_size), interpolation=cv2.INTER_AREA)
-            lr_img = cv2.resize(hr_img, (self.lr_img_size, self.lr_img_size), interpolation=cv2.INTER_AREA)
-            #lr_img = cv2.resize(lr_img,(self.hr_img_size, self.hr_img_size), interpolation=cv2.INTER_CUBIC)
-
-            if self.transforms:
-                transformed = self.transforms(image=lr_img, label=hr_img)
-                lr_img = transformed['image'] / 255.
-                hr_img = transformed['label'] / 255.
+            hr_img = random_crop(hr_img, self.hr_size)
+            
+            lr_img = cv2.resize(hr_img, (0,0),fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
+            
+            transformed = self.transforms(image=lr_img, label=hr_img)
+            lr_img = transformed['image'] / 255.
+            hr_img = transformed['label'] / 255.   
             
             return lr_img, hr_img
+        
         else: # self.mode == "test"
-            # test mode learning with lr == 512*512 -> 64*64 -> divided 8
-            # TODO: must check split img operates well
-
             lr_path = os.path.join(self.root_path, self.df['LR'].iloc[index]) 
             lr_img = cv2.imread(lr_path)
             lr_img = cv2.resize(lr_img, (512, 512), interpolation=cv2.INTER_CUBIC)
 
-            #TODO: make split img list
+            
             split_imgs = []
-            for i in range(0, 512, 64):
-                for j in range(0, 512, 64):
-                    split_imgs.append(lr_img[i:i+64, j:j+64])
+            for i in range(0, 512, self.lr_size):
+                for j in range(0, 512, self.lr_size):
+                    split_imgs.append(lr_img[i:i+self.lr_size, j:j+self.lr_size])
 
             file_name = lr_path.split('/')[-1]
             split_tensors = []
-            if self.transforms is not None:
-                for s_img in split_imgs:
-                    transformed = self.transforms(image=s_img)
-                    split_tensors.append(transformed['image'] / 255.)
+            for s_img in split_imgs:
+                transformed = self.transforms(image=s_img)
+                split_tensors.append(transformed['image'] / 255.)
 
             return split_tensors, file_name
         
     def __len__(self):
         return len(self.df)
-
+    
 def get_train_transform():
     return A.Compose([
-        ToTensorV2(p=1.0)],
+        A.HorizontalFlip(p=0.5),
+        A.RandomRotate90(always_apply=True),
+        ToTensorV2(p=1.0)
+        ],
         additional_targets={'image': 'image', 'label': 'image'}
     )
+
 
 def get_test_transform():
     return A.Compose([
         ToTensorV2(p=1.0)],
         additional_targets={'image': 'image', 'label': 'image'}
     )
+
+
+def random_crop(img, size):
+    h, w, c = img.shape
+
+    h_start = np.random.randint(0, h - size)
+    h_end = h_start + size
+
+    w_start = np.random.randint(0, w - size)
+    w_end = w_start + size
+
+    return img[h_start:h_end, w_start:w_end]
+
+
--- code/train.py
+++ code/train.py
@@ -2,24 +2,39 @@ import numpy as np
 from torch import nn
 from tqdm.auto import tqdm
 import torch
+import math
+import os
 
 
-def train(model, train_loader, val_loader, optimizer, criterion, scheduler, device, save_path, max_epoch=300, early_stop=10):
+def psnr(label, outputs, max_val=1.):
+    label = label.cpu().detach().numpy()
+    outputs = outputs.cpu().detach().numpy()
+    img_diff = outputs - label
+    rmse = math.sqrt(np.mean((img_diff)**2))
+    if rmse == 0:  # label과 output이 완전히 일치하는 경우
+        return 100
+    else:
+        psnr = 20 * math.log10(max_val/rmse)
+        return psnr
+
+
+def train(model, model_name, train_loader, val_loader, optimizer, criterion, scheduler, device, save_path, max_epoch=300, early_stop=10):
     model.to(device)
     criterion.to(device)
-
     best_model = None
     best_loss = float('inf')
     check_early_stop = 0
     _train_loss = False
     _val_loss = False
+    _val_psnr = 0
     train_loss_list = []
     val_loss_list = []
+    psnr_list = []
 
     for epoch in range(1, max_epoch):
         # training
         model.train()
-        
+
         train_loss_list.clear()
         for lr_img, hr_img in tqdm(iter(train_loader)):
             lr_img, hr_img = lr_img.float().to(device), hr_img.float().to(device)
@@ -43,25 +58,30 @@ def train(model, train_loader, val_loader, optimizer, criterion, scheduler, devi
         # validation
         model.eval()
         val_loss_list.clear()
+        psnr_list.clear()
         with torch.no_grad():
-            
+
             for lr_img, hr_img in tqdm(iter(val_loader)):
                 lr_img, hr_img = lr_img.float().to(device), hr_img.float().to(device)
 
                 pred_hr_img = model(lr_img)
                 loss = criterion(pred_hr_img, hr_img)
 
+                psnr_list.append(psnr(label=hr_img, outputs=pred_hr_img))
                 val_loss_list.append(float(loss.item()))
 
             _val_loss = np.mean(val_loss_list)
+            _val_psnr = np.mean(psnr_list)
 
             if best_loss > _val_loss:
                 best_loss = _val_loss
                 best_model = model
+                save_name = f"{model_name}_{epoch}_{_val_loss:.3f}.pt"
+                save_file_path = os.path.join(save_path, save_name)
                 torch.save({"state_dict": model.module.state_dict(),
                             'loss': _val_loss,
                             'epoch': epoch},
-                            save_path)
+                           save_file_path)
                 check_early_stop = 0
                 # print("Minist Val Loss")
             else:
@@ -71,6 +91,6 @@ def train(model, train_loader, val_loader, optimizer, criterion, scheduler, devi
                     break
 
         print(
-            f'Epoch:{epoch}, Train Loss:{_train_loss:.5f}, Val Loss:{_val_loss:.5f}, es:{check_early_stop}')
+            f'Epoch:{epoch}, Train Loss:{_train_loss:.5f}, Val Loss:{_val_loss:.5f}, Val PSNR: {_val_psnr}, es:{check_early_stop}')
 
     return best_model
